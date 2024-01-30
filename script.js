04a3ea18fb267bb8031a6780b9d0253e57701e32
Ashrarul Sifat <sifatashrarul@gmail.com>
2024-01-31 03:13:00 +0600

    week-04 completed

--- script.js
+++ script.js
@@ -5,20 +5,7 @@ function toggleMenu() {
     icon.classList.toggle("open")
 } 
 
-$(document).ready(function(){
 
-    var typed = new Typed(".typing", {
-        strings: ["Teacher", "Researcher", "Photographer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-});
-
-function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
 
 
 window.onscroll = function() {
@@ -35,68 +22,6 @@ window.onscroll = function() {
 function sendEmail(){  
     var name=document.getElementById("name").value;  
     var email=document.getElementById("email").value;  
-    var subject=document.getElementById("subject").value;  
     var body=document.getElementById("body").value;
-    window.open('mailto:saeed.utsha@gmail.com?'+'&subject='+subject+'&body='+body);  
-}
-
-showPublications("All");
-showProjects('Allp');
-
-function showPublications(year) {
-    var allContent = document.querySelectorAll("#publicationContent > ul");
-
-    var buttons = document.querySelectorAll('.button-publications');
-    buttons.forEach(function(button) {
-        button.classList.remove('selected');
-    });
-
-    if (year === "All") {
-        allContent.forEach(function(content) {
-            content.style.display = "block";
-        });
-        document.getElementById("btnAll").classList.add('selected');
-    } else {
-        allContent.forEach(function(content) {
-            if (content.id === "year" + year) {
-                content.style.display = "block";
-                document.getElementById("btn" + year).classList.add('selected');
-            } else {
-                content.style.display = "none";
-            }
-        });
-    }
-}
-
-
-function showProjects(topic) {
-    var allContent = document.querySelectorAll("#projectContent > ul");
-
-    var buttons = document.querySelectorAll('.button-projects');
-    buttons.forEach(function(button) {
-        button.classList.remove('selected');
-    });
-
-    if (topic === "Allp") {
-        allContent.forEach(function(content) {
-            content.style.display = "block";
-        });
-        document.getElementById("btnAllp").classList.add('selected');
-    } else {
-        allContent.forEach(function(content) {
-            if (content.id === "pr" + topic) {
-                content.style.display = "block";
-                document.getElementById("btn" + topic).classList.add('selected'); 
-            }
-            
-            else if (content.id === "prBoth") {
-                content.style.display = "block";
-                document.getElementById("btn" + topic).classList.add('selected'); 
-            }
-            
-            else {
-                content.style.display = "none";
-            }
-        });
-    }
+    window.open('index.html');  
 }
